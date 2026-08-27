a2852921b24a58ca15273755d0e0c03074991a96
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-27 21:32:40 +0300

    Auto update 2026-08-27 21:32:40

--- src/main/java/net/mcreator/lifemod/client/PlayerModelHider.java
+++ src/main/java/net/mcreator/lifemod/client/PlayerModelHider.java
@@ -157,7 +157,7 @@ public class PlayerModelHider {
                     poseStack,
                     event.getMultiBufferSource(),
                     event.getPackedLight(),
-                    event.getPackedOverlay(),
+                    0,
                     event.getPartialTick()
             );
 
