03fbcd7105154a13386663c38f360f017aefbe57
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-27 21:42:59 +0300

    Auto update 2026-08-27 21:42:58

--- src/main/java/net/mcreator/lifemod/client/PlayerCustomRenderer.java
+++ src/main/java/net/mcreator/lifemod/client/PlayerCustomRenderer.java
@@ -2,17 +2,22 @@ package net.mcreator.lifemod.client;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
+
 import software.bernie.geckolib.renderer.GeoObjectRenderer;
 
-public class PlayerCustomRenderer extends GeoObjectRenderer<PlayerModelAnimatable> {
+public class PlayerCustomRenderer
+        extends GeoObjectRenderer<PlayerModelAnimatable> {
 
     private final boolean male;
 
     public PlayerCustomRenderer(boolean male) {
+
         super(new PlayerCustomModel(male));
+
         this.male = male;
     }
 
@@ -24,19 +29,28 @@ public class PlayerCustomRenderer extends GeoObjectRenderer<PlayerModelAnimatabl
             int packedOverlay,
             float partialTick
     ) {
+
         animatable.updateMovement(partialTick);
 
-        ResourceLocation texture = male
-                ? new ResourceLocation(
-                        "life_mod",
-                        "textures/entity/custom_male.png"
-                )
-                : new ResourceLocation(
-                        "life_mod",
-                        "textures/entity/custom_female.png"
-                );
+        ResourceLocation texture;
 
-        RenderType renderType = RenderType.entityTranslucent(texture);
+        if (male) {
+
+            texture = new ResourceLocation(
+                    "life_mod",
+                    "textures/entity/custom_male.png"
+            );
+
+        } else {
+
+            texture = new ResourceLocation(
+                    "life_mod",
+                    "textures/entity/custom_female.png"
+            );
+        }
+
+        RenderType renderType =
+                RenderType.entityTranslucent(texture);
 
         VertexConsumer vertexConsumer =
                 bufferSource.getBuffer(renderType);
