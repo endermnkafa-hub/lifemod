a2cfdb04784d265dd27e2f42901f9e5b953c8119
endermnkafa-hub <endermnkafa-hub@users.noreply.github.com>
2026-08-27 20:43:37 +0300

    Auto update 2026-08-27 20:43:37

--- src/main/java/net/mcreator/lifemod/client/PlayerCustomModel.java
+++ src/main/java/net/mcreator/lifemod/client/PlayerCustomModel.java
@@ -1,32 +1,46 @@
 package net.mcreator.lifemod.client;
 
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.RenderType;
 import net.minecraft.resources.ResourceLocation;
-import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
-import software.bernie.geckolib.cache.object.BakedGeoModel;
-import software.bernie.geckolib.core.animatable.GeoAnimatable;
-import software.bernie.geckolib.core.animation.AnimatableManager;
-import software.bernie.geckolib.core.animation.AnimationController;
-import software.bernie.geckolib.core.animation.AnimationState;
 import software.bernie.geckolib.model.GeoModel;
-import software.bernie.geckolib.renderer.GeoEntityRenderer;
 
 public class PlayerCustomModel extends GeoModel<PlayerModelAnimatable> {
-    private final ResourceLocation modelResource;
-    private final ResourceLocation textureResource;
+
+    private final boolean isMale;
 
     public PlayerCustomModel(boolean isMale) {
-        this.modelResource = new ResourceLocation("life_mod", isMale ? "geo/male.geo.json" : "geo/female.geo.json");
-        this.textureResource = new ResourceLocation("life_mod", "textures/entity/" + (isMale ? "male.png" : "female.png"));
+        this.isMale = isMale;
     }
 
     @Override
-    public ResourceLocation getModelResource(PlayerModelAnimatable animatable) { return modelResource; }
+    public ResourceLocation getModelResource(PlayerModelAnimatable animatable) {
+
+        return new ResourceLocation(
+                "life_mod",
+                isMale
+                        ? "geo/custom_male.geo.json"
+                        : "geo/custom_female.geo.json"
+        );
+    }
+
     @Override
-    public ResourceLocation getTextureResource(PlayerModelAnimatable animatable) { return textureResource; }
+    public ResourceLocation getTextureResource(PlayerModelAnimatable animatable) {
+
+        return new ResourceLocation(
+                "life_mod",
+                isMale
+                        ? "textures/entity/custom_male.png"
+                        : "textures/entity/custom_female.png"
+        );
+    }
+
     @Override
-    public ResourceLocation getAnimationResource(PlayerModelAnimatable animatable) { return new ResourceLocation("life_mod", "animations/" + (animatable.isMale() ? "male.animation.json" : "female.animation.json")); }
+    public ResourceLocation getAnimationResource(PlayerModelAnimatable animatable) {
+
+        return new ResourceLocation(
+                "life_mod",
+                isMale
+                        ? "animations/male.animation.json"
+                        : "animations/female.animation.json"
+        );
+    }
 }
